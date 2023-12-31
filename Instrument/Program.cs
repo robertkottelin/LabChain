7db7b4430d8fe9c0c0025362b713e3a6dc70108e
robertkottelin <robert.kottelin@gmail.com>
2023-12-31 10:15:27 +0100

    .

---
+++ Instrument/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+
+namespace Instrument
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string logFilePath = "/home/robert/LabChain/instrument_log.csv";
+            GenerateLogFile(logFilePath);
+            Console.WriteLine($"Log file generated at {logFilePath}");
+        }
+
+        static void GenerateLogFile(string filePath)
+        {
+            using (StreamWriter writer = new StreamWriter(filePath))
+            {
+                writer.WriteLine("Timestamp, Temperature, Pressure"); // Header
+
+                Random random = new Random();
+                for (int i = 0; i < 1000; i++) // Generate 100 log entries
+                {
+                    string timestamp = DateTime.Now.AddMinutes(i * 5).ToString("yyyy-MM-dd HH:mm:ss");
+                    // random temperature between 20 and 25
+                    double temperature = 20 + random.NextDouble() * 5;
+                    double pressure = random.NextDouble() * 2;
+                    writer.WriteLine($"{timestamp}, {temperature:F2}, {pressure:F2}");
+                }
+            }
+        }
+    }
+}
---
+++ LabChainApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+
+namespace LabChainApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string logFilePath = "/home/robert/LabChain/instrument_log.csv";
+            GenerateLogFile(logFilePath);
+            Console.WriteLine($"Log file generated at {logFilePath}");
+        }
+
+        static void ReadLogFile(filepath)
+        {
+        }
+    }
+}
